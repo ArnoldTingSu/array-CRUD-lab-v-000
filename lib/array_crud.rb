c1e3633406535d1bab8851c39a9b78e78545337b
Arnold Su <arnoldtingsu@gmail.com>
2018-10-10 22:25:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array
-  array = Array.new << "newitem"
+  array = Array.new
+  array.unshift("new_item")
 end
 
 def add_element_to_start_of_array(array, element)
